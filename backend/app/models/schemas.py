1722e4d48b2bf8eb6f06356a2d3c488036b4c586
Angel Pauly <angelpauly72@gmail.com>
2026-04-22 23:47:06 +0530

    updated schemas

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -9,3 +9,11 @@ class InterviewRequest(BaseModel):
 class AnswerRequest(BaseModel):
     answer: str
     question: str
+
+class UserSignup(BaseModel):
+    email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
