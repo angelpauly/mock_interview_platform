47902968895bc18735a37181f144f69cc3ea5882
Angel Pauly <angelpauly72@gmail.com>
2026-04-28 12:05:32 +0530

    add login + remove venv

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function Login({ setIsLoggedIn }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await fetch("http://127.0.0.1:8000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+
+    if (data.access_token) {
+      localStorage.setItem("token", data.access_token);
+      setIsLoggedIn(true);
+    } else {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <br /><br />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <br /><br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
