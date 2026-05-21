e2f1f4e6a10cd121a1f93c3af5c5d6bf8af01075
Angel Pauly <angelpauly72@gmail.com>
2026-05-21 21:37:33 +0530

    added instructions component

---
+++ frontend/src/components/Instruction.jsx
@@ -0,0 +1,9 @@
+<>
+  <h3>Instructions</h3>
+  <p>You will be asked 10 questions.</p>
+  <p>Answer carefully. All the best! 🚀</p>
+
+  <button onClick={fetchQuestion}>
+    Start Practice
+  </button>
+</>
