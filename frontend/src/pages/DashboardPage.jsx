b08b6bde7cc00933d9cf92aff9e07a6b3bf121f9
Angel Pauly <angelpauly72@gmail.com>
2026-05-27 23:16:34 +0530

    added dashboard page

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,15 @@
+import Dashboard from "../components/Dashboard";
+
+function DashboardPage() {
+
+    return (
+
+        <div className="min-h-screen bg-gray-100 p-10">
+
+            <Dashboard />
+
+        </div>
+    );
+}
+
+export default DashboardPage;
